b7a06e0d83e577d774cc72859533540b03dc7f33
aixatatiane <tatiane.f@escolar.ifrn.edu.br>
2024-08-02 00:31:04 -0300

    Adiciona URL e template do autor

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.template import loader
+
+def t
 
 urlpatterns = [
     path('admin/', admin.site.urls),
